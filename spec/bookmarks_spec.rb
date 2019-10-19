2a683e13977374d7885778b7cbd27406dfe49846
Chris Groves <chris_groves87@hotmail.co.uk>
2019-10-19 10:51:17 +0100

    feat: use env variable to run tests from test database

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -3,7 +3,14 @@ require './lib/bookmark'
 describe Bookmark do
   describe '.all' do
     it 'can return a list of bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('www.google.com');")
+
       bookmarks = Bookmark.all
+      
       expect(bookmarks).to include('www.makersacademy.com')
       expect(bookmarks).to include('www.destroyallsoftware.com')
       expect(bookmarks).to include('www.google.com')
