b800d1f3bc631677a22abcd5ec0b6302ccc7372e
Chris Groves <chris_groves87@hotmail.co.uk>
2019-11-12 15:47:05 +0000

    refactor: add query method to DatabaseConnection class

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -13,4 +13,14 @@ describe DatabaseConnection do
       connection = DatabaseConnection.setup('bookmark_manager_test')
       expect(DatabaseConnection.connection).to eq connection
     end
+
+  describe '.query' do
+    it 'executes a query via PG' do
+      connection = DatabaseConnection.setup('bookmark_manager_test')
+
+      expect(connection).to receive(:exec).with("SELECT * FROM bookmarks;")
+
+      DatabaseConnection.query("SELECT * FROM bookmarks;")
+    end
+  end
 end
